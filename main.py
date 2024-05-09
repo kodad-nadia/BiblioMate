17ccd2da04fba60f681ad45743e71af23509ea7a
kodad-nadia <106157374+kodad-nadia@users.noreply.github.com>
2024-05-09 22:24:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 
 # Importing the routers
 import routers.router_tasks
-import routers.router_auth
+
 
 
 # Initializing the API
@@ -15,7 +15,6 @@ app = FastAPI(
 
 # Including routers dedicated to managing tasks
 app.include_router(routers.router_tasks.router)
-app.include_router(routers.router_auth.router)
 
 
 
