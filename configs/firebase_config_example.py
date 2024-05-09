abb03d5c6415698156e616a859b8a139aff4084c
kodad-nadia <106157374+kodad-nadia@users.noreply.github.com>
2024-05-09 22:34:15 +0200

    Update and rename firebase_config_example.py to firebase_config.py

--- configs/firebase_config_example.py
+++
@@ -1,9 +0,0 @@
-firebaseConfig = {
-  "apiKey": "AIzaSyBVkadoTEkyPy7--a7iEMXx3ZRq899TSOs",
-  "authDomain": "myapi-adcde.firebaseapp.com",
-  "projectId": "myapi-adcde",
-  "storageBucket": "myapi-adcde.appspot.com",
-  "messagingSenderId": "21113848465",
-  "appId": "1:21113848465:web:05fcf777cdd319e2b97530",
-  "databaseURL": "https://myapi-adcde-default-rtdb.europe-west1.firebasedatabase.app"
-}
